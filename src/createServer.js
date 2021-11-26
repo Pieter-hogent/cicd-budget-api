829302d9f1fcf4ab242160792aef94330893b830
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-11-26 17:57:42 +0100

    output which port we're listening

--- src/createServer.js
+++ src/createServer.js
@@ -52,8 +52,9 @@ module.exports = async function createServer() {
 
 		start() {
 			return new Promise((resolve) => {
-				app.listen(process.env.port || 9000);
-				logger.info(`🚀 Server listening on http://localhost:9000`);
+				const port = process.env.port || 9000;
+				app.listen(port);
+				logger.info(`🚀 Server listening on http://localhost:${port}`);
 				resolve();
 			});
 		},

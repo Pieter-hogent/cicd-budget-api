5281b7a67556e989f87217c0959d8c1bdfab96da
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-11-29 14:33:42 +0100

    clean up cors

--- config/production.js
+++ config/production.js
@@ -6,7 +6,6 @@ module.exports = {
 	cors: {
 		origins: [
 			'http://localhost:3000',
-			'https://pieter-hogent.github.io/frontendweb-budget/',
 			'https://pieter-hogent.github.io/',
 		],
 		maxAge: 3 * 60 * 60, // 3h in seconds

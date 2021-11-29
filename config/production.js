c13ef743e9173020f5674a6f69c7ce46600a80c0
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-11-29 12:45:49 +0100

    add githubpages url to cors

--- config/production.js
+++ config/production.js
@@ -4,19 +4,22 @@ module.exports = {
 		disabled: false,
 	},
 	cors: {
-		origins: ['http://localhost:3000'],
+		origins: [
+			'http://localhost:3000',
+			'https://pieter-hogent.github.io/frontendweb-budget/',
+		],
 		maxAge: 3 * 60 * 60, // 3h in seconds
 	},
 	database: {
-    client: 'mysql2',
+		client: 'mysql2',
 		host: 'localhost',
-    port: 3306,
-    name: 'budget',
-  },
+		port: 3306,
+		name: 'budget',
+	},
 	pagination: {
-    limit: 100,
-    offset: 0,
-  },
+		limit: 100,
+		offset: 0,
+	},
 	auth: {
 		argon: {
 			saltLength: 16,
@@ -30,5 +33,5 @@ module.exports = {
 			issuer: 'budget.hogent.be',
 			audience: 'budget.hogent.be',
 		},
-	}
+	},
 };

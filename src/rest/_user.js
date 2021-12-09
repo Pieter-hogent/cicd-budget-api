f21bafa79c8cebd4df0e9f584e04a129a2d05d9c
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2021-12-09 09:40:12 +0100

    explicit status code

--- src/rest/_user.js
+++ src/rest/_user.js
@@ -12,29 +12,30 @@ const login = async (ctx) => {
 const register = async (ctx) => {
 	const session = await userService.register(ctx.request.body);
 	ctx.body = session;
+	ctx.status = 200;
 };
 
 const getAllUsers = async (ctx) => {
-  const users = await userService.getAll(
-    ctx.query.limit && Number(ctx.query.limit),
-    ctx.query.offset && Number(ctx.query.offset),
-  );
-  ctx.body = users;
+	const users = await userService.getAll(
+		ctx.query.limit && Number(ctx.query.limit),
+		ctx.query.offset && Number(ctx.query.offset)
+	);
+	ctx.body = users;
 };
 
 const getUserById = async (ctx) => {
-  const user = await userService.getById(ctx.params.id);
-  ctx.body = user;
+	const user = await userService.getById(ctx.params.id);
+	ctx.body = user;
 };
 
 const updateUserById = async (ctx) => {
-  const user = await userService.updateById(ctx.params.id, ctx.request.body);
-  ctx.body = user;
+	const user = await userService.updateById(ctx.params.id, ctx.request.body);
+	ctx.body = user;
 };
 
 const deleteUserById = async (ctx) => {
-  await userService.deleteById(ctx.params.id);
-  ctx.status = 204;
+	await userService.deleteById(ctx.params.id);
+	ctx.status = 204;
 };
 
 /**
@@ -43,23 +44,21 @@ const deleteUserById = async (ctx) => {
  * @param {Router} app - The parent router.
  */
 module.exports = function installUsersRoutes(app) {
-  const router = new Router({
-    prefix: '/users',
-  });
+	const router = new Router({
+		prefix: '/users',
+	});
 
-  // Public routes
+	// Public routes
 	router.post('/login', login);
 	router.post('/register', register);
 
-  const requireAdmin = makeRequireRole(Role.ADMIN);
-  
-  // Routes with authentication/autorisation
-  router.get('/', requireAuthentication, requireAdmin, getAllUsers);
-  router.get('/:id', requireAuthentication, getUserById);
-  router.put('/:id', requireAuthentication, updateUserById);
-  router.delete('/:id', requireAuthentication, deleteUserById);
+	const requireAdmin = makeRequireRole(Role.ADMIN);
 
-  app
-    .use(router.routes())
-    .use(router.allowedMethods());
+	// Routes with authentication/autorisation
+	router.get('/', requireAuthentication, requireAdmin, getAllUsers);
+	router.get('/:id', requireAuthentication, getUserById);
+	router.put('/:id', requireAuthentication, updateUserById);
+	router.delete('/:id', requireAuthentication, deleteUserById);
+
+	app.use(router.routes()).use(router.allowedMethods());
 };
